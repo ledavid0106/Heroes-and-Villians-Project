64064c70e5da68acfd54002f4045d46b8e4980e3
David Le <104813594+ledavid0106@users.noreply.github.com>
2022-10-10 13:45:32 -0700

    Added the functionality of the POST in supers_list

--- supers/views.py
+++ supers/views.py
@@ -7,10 +7,22 @@ from .serializers import SuperSerializer
 
 from .models import Super
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def supers_list(request):
 
     if request.method == 'GET':
         supers = Super.objects.all()
         serializer = SuperSerializer(supers, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = SuperSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status = status.HTTP_201_CREATED)
+
+@api_view(['GET'])
+def supers_detail(request, pk):
+    supers = get_object_or_404(Super, pk=pk)
+    if request.method == 'GET':
+        serializer = SuperSerializer(supers)
         return Response(serializer.data)
