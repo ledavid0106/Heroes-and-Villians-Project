3c0e3809adeab3db33afce35f86f5654fc506f09
David Le <104813594+ledavid0106@users.noreply.github.com>
2022-10-11 12:04:33 -0700

    Created the functionality of a custom dictionary for the types of Heroes

--- supers/views.py
+++ supers/views.py
@@ -11,14 +11,26 @@ from .models import Super
 def supers_list(request):
 
     if request.method == 'GET':
-        type_param = request.query_params.get('type')
-        if type_param:
-            query_set = Super.objects.filter(super_type__type = type_param)
+        super_type_param = request.query_params.get('type')
+        if super_type_param:
+            query_set = Super.objects.filter(super_type__type = super_type_param)
             serializer = SuperSerializer(query_set, many = True)
             return Response(serializer.data, status= status.HTTP_200_OK)
-        supers = Super.objects.all()
-        serializer = SuperSerializer(supers, many=True)
-        return Response(serializer.data) 
+        super_types = SuperType.objects.all()
+        custom_response = {}
+        for type in super_types:
+            supers = Super.objects.filter(super_type = type.id)
+            super_serialzier = SuperSerializer(supers, many = True)
+
+            custom_response[type.type] = {
+                "Supers": super_serialzier.data
+            }
+
+        return Response(custom_response)    
+        #for if you dont want to use the custom dictionary    
+            # supers = Super.objects.all()
+            # serializer = SuperSerializer(supers, many=True)
+            # return Response(serializer.data) 
 
     elif request.method == 'POST':
         serializer = SuperSerializer(data=request.data)
